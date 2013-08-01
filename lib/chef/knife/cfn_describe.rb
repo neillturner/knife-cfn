e76eac26c0fd29e3b42607b858303c4bd693d189
brettcave <brett@cave.za.net>
2013-08-01 14:32:16 +0200

    default the "describe" output mode to "Stack Name" and add an option to display Stack ARNs (-l / --long). Describe now also includes the stack output, which may be useful to reference for other knife commands (e.g.if a stack outputs an ELB DNSName, and another stack uses that as a parameter).

--- lib/chef/knife/cfn_describe.rb
+++ lib/chef/knife/cfn_describe.rb
@@ -18,40 +18,48 @@ require 'chef/knife/cfn_base'
 class Chef
   class Knife
     class CfnDescribe < Chef::Knife::CfnBase
-    
+
       deps do
         require 'fog'
         require 'readline'
         require 'chef/json_compat'
         require 'chef/knife/bootstrap'
         Chef::Knife::Bootstrap.load_deps
-      end    
+      end
 
       banner "knife cfn describe [stack name] - lists all stacks if [stack name] is omitted."
+
+      option :long_names,
+             :short  => "-l",
+             :long   => "--long",
+             :description  => "Use long stack names (ARN) instead of friendly names"
 
       def run
         $stdout.sync = true
 
         validate!
-        
-        stack_name = @name_args[0]
-        output_mode = "StackId"
-        output_header = "Stack ID"
 
-		
-	if stack_name.nil?
-    @name_args[0] = "__ALL__"
-    output_mode = "StackName"
-    output_header = "Stack Name"
-	end
+        stack_name = @name_args[0]
+        if stack_name.nil?
+          @name_args[0] = "__ALL__"
+        end
+
+        output_mode = "StackName"
+        output_header = "Stack Name"
+
+        if !config[:long_names].nil?
+          output_mode = "StackId"
+          output_header = "Stack ID"
+        end
 
         stack_list = [
-          ui.color(output_header, :bold),
-          ui.color('Status', :bold),
-          ui.color('Creation Time', :bold),
-          ui.color('Disable Rollback', :bold)
+            ui.color(output_header, :bold),
+            ui.color('Status', :bold),
+            ui.color('Output', :bold),
+            ui.color('Creation Time', :bold),
+            ui.color('Disable Rollback', :bold)
         ]
-        
+
         @name_args.each do |stack_name|
           options = {}
           data = Array.new
@@ -65,26 +73,35 @@ class Chef
             end
 
             data = response.body['Stacks']
-            rescue Excon::Errors::BadRequest => e
-              i= e.response.body.index("<Message>")
-              j = e.response.body.index("</Message>")
-              if !i.nil? and !j.nil?
-                 ui.error(e.response.body[i+9,j-i-9])
-              else
-                 print "\n#{e.response.body}"
-              end
-              exit 1
+          rescue Excon::Errors::BadRequest => e
+            i= e.response.body.index("<Message>")
+            j = e.response.body.index("</Message>")
+            if !i.nil? and !j.nil?
+              ui.error(e.response.body[i+9,j-i-9])
             else
-	      data.each do |stack|
-                stack_list << stack[output_mode]
-                stack_list << stack['StackStatus']
-                stack_list << stack['CreationTime'].to_s
-                stack_list << stack['DisableRollback'].to_s
+              print "\n#{e.response.body}"
+            end
+            exit 1
+          else
+            data.each do |stack|
+              stack_output = "|"
+              stack["Outputs"].each do |output|
+                output_key=output['OutputKey']
+                output_value=output['OutputValue']
+                stack_output += "#{output_key}:#{output_value}; "
               end
-              puts ui.list(stack_list, :uneven_columns_across, 4)
-            end  
+              stack_output+="|"
+              stack_list << stack[output_mode]
+              stack_list << stack['StackStatus']
+              stack_list << stack_output
+              stack_list << stack['CreationTime'].to_s
+              stack_list << stack['DisableRollback'].to_s
+            end
+
+            puts ui.list(stack_list, :uneven_columns_across, 5)
           end
         end
       end
     end
+  end
 end
